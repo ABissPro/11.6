572526a809c2e1ce2ab56d56c75f7787c37a3c45
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-18 19:31:41 +0300

    Add files via upload

---
+++ 1. Шифр Цезаря.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+
+
+
+std::string encrypt_caesar(const std::string& text, int shift) {
+    std::string result;
+    for (char c : text) {
+        if (c >= 'a' && c <= 'z') {
+            result += 'a' + (c - 'a' + shift) % 26;
+        }
+        else if (c >= 'A' && c <= 'Z') {
+            result += 'A' + (c - 'A' + shift) % 26;
+        }
+        else {
+            result += c;
+        }
+    }
+    return result;
+}
+
+int main() {
+    system("chcp 1251"); // Установить кодировку
+    std::string text;
+    int shift;
+    std::cout << "Введите текст и шаг кодировки:\n";
+    std::cin >> text >> shift;
+    std::string encrypted = encrypt_caesar(text, shift);
+    std::cout << "Зашифрованный текст: " << encrypted << std::endl;
+    return 0;
+}
