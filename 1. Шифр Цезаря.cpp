ac0b1f8fc407c3553b11d066bb689f6aac5ee1f0
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-22 11:48:02 +0300

    Update 1. Шифр Цезаря.cpp

--- 1. Шифр Цезаря.cpp
+++ 1. Шифр Цезаря.cpp
@@ -7,10 +7,12 @@ std::string encrypt_caesar(const std::string& text, int shift) {
     std::string result;
     for (char c : text) {
         if (c >= 'a' && c <= 'z') {
-            result += 'a' + (c - 'a' + shift) % 26;
+            int adjustedShift = (shift % 26 + 26) % 26;
+            result += 'a' + (c - 'a' + adjustedShift) % 26;
         }
         else if (c >= 'A' && c <= 'Z') {
-            result += 'A' + (c - 'A' + shift) % 26;
+            int adjustedShift = (shift % 26 + 26) % 26;
+            result += 'A' + (c - 'A' + adjustedShift) % 26;
         }
         else {
             result += c;
